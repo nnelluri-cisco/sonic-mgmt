2ab053ca139f18a999de7e51198970039add6d52
nnelluri <nnelluri@cisco.com>
2026-04-13 21:33:49 -0700

    changes scope from module to function
    Signed-off-by: nnelluri <nnelluri@cisco.com>

--- tests/ha/test_ha_npu_process_crash.py
+++ tests/ha/test_ha_npu_process_crash.py
@@ -456,7 +456,7 @@ class TestNpuProcessCrash:
     def test_crash_active_npu_traffic_on_active(
         self, process_name, container,
         primary_dut, standby_dut, duthosts,
-        setup_ha_config, setup_dash_ha_from_json,
+        setup_ha_config, setup_dash_ha_from_json_func_scope,
         setup_gnmi_server, setup_dash_pl_pipeline,
         ptfadapter, dash_pl_config,
         localhost, ptfhost,
@@ -479,7 +479,7 @@ class TestNpuProcessCrash:
     def test_crash_active_npu_traffic_on_standby(
         self, process_name, container,
         primary_dut, standby_dut, duthosts,
-        setup_ha_config, setup_dash_ha_from_json,
+        setup_ha_config, setup_dash_ha_from_json_func_scope,
         setup_gnmi_server, setup_dash_pl_pipeline,
         ptfadapter, dash_pl_config,
         localhost, ptfhost,
@@ -502,7 +502,7 @@ class TestNpuProcessCrash:
     def test_crash_standby_npu_traffic_on_active(
         self, process_name, container,
         primary_dut, standby_dut, duthosts,
-        setup_ha_config, setup_dash_ha_from_json,
+        setup_ha_config, setup_dash_ha_from_json_func_scope,
         setup_gnmi_server, setup_dash_pl_pipeline,
         ptfadapter, dash_pl_config,
         localhost, ptfhost,
@@ -525,7 +525,7 @@ class TestNpuProcessCrash:
     def test_crash_standby_npu_traffic_on_standby(
         self, process_name, container,
         primary_dut, standby_dut, duthosts,
-        setup_ha_config, setup_dash_ha_from_json,
+        setup_ha_config, setup_dash_ha_from_json_func_scope,
         setup_gnmi_server, setup_dash_pl_pipeline,
         ptfadapter, dash_pl_config,
         localhost, ptfhost,
